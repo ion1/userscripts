4e60c9d6069d62537ad2e64058e03356413279fe
Johan Kiviniemi <devel@johan.kiviniemi.name>
2023-03-11 17:51:16 +0200

    Add updateURL and downloadURL metadata

--- packages/youtube-mute-skip-ads/vite.config.ts
+++ packages/youtube-mute-skip-ads/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import monkey from "vite-plugin-monkey";
 
+const canonicalUrl =
+  "https://ion1.github.io/userscripts/youtube-mute-skip-ads.user.js";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -14,6 +17,8 @@ export default defineConfig({
         namespace: "https://github.com/ion1/userscripts",
         match: ["*://www.youtube.com/*", "*://music.youtube.com/*"],
         icon: "https://www.google.com/s2/favicons?sz=64&domain=youtube.com",
+        updateURL: canonicalUrl,
+        downloadURL: canonicalUrl,
         // Display any post-reload notification ASAP.
         "run-at": "document-body",
       },
