8b70a062df988807b50d03ef8d4da07a9f4696b3
Johan Kiviniemi <devel@johan.kiviniemi.name>
2023-03-22 01:26:05 +0200

    Add shared eslint-config

---
+++ packages/eslint-config/eslint-config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
