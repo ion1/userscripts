b8eb5f8d40b3f76b8efb1a329c5d28e50f244393
Johan Kiviniemi <devel@johan.kiviniemi.name>
2024-08-25 09:35:49 +0300

    youtube-mute-skip-ads: Watcher: Mark things private

--- packages/youtube-mute-skip-ads/src/watcher.ts
+++ packages/youtube-mute-skip-ads/src/watcher.ts
@@ -23,71 +23,67 @@ export class Watcher {
 
   element: HTMLElement | null;
 
-  onCreatedCallbacks: OnCreatedCallback[];
-  onRemovedCallbacks: OnRemovedCallback[];
+  #onCreatedCallbacks: OnCreatedCallback[];
+  #onRemovedCallbacks: OnRemovedCallback[];
 
-  nodeObserver: MutationObserver | null;
-  nodeWatchers: { selector: SelectorType; name: string; watcher: Watcher }[];
+  #nodeObserver: MutationObserver | null;
+  #nodeWatchers: { selector: SelectorType; name: string; watcher: Watcher }[];
 
-  textObserver: MutationObserver | null;
-  onTextChangedCallbacks: OnTextChangedCallback[];
+  #textObserver: MutationObserver | null;
+  #onTextChangedCallbacks: OnTextChangedCallback[];
 
-  onAttrChangedCallbacks: {
+  #onAttrChangedCallbacks: {
     name: string;
     callback: OnAttrChangedCallback;
     observer: MutationObserver | null;
   }[];
 
   visibilityAncestor: HTMLElement | null;
-  visibilityObserver: IntersectionObserver | null;
-  isVisible: boolean | null;
-  visibilityWatchers: Watcher[];
+  #visibilityObserver: IntersectionObserver | null;
+  #isVisible: boolean | null;
+  #visibilityWatchers: Watcher[];
 
   constructor(name: string, elem?: HTMLElement) {
     this.name = name;
 
     this.element = null;
 
-    this.onCreatedCallbacks = [];
-    this.onRemovedCallbacks = [];
+    this.#onCreatedCallbacks = [];
+    this.#onRemovedCallbacks = [];
 
-    this.nodeObserver = null;
-    this.nodeWatchers = [];
+    this.#nodeObserver = null;
+    this.#nodeWatchers = [];
 
-    this.textObserver = null;
-    this.onTextChangedCallbacks = [];
+    this.#textObserver = null;
+    this.#onTextChangedCallbacks = [];
 
-    this.onAttrChangedCallbacks = [];
+    this.#onAttrChangedCallbacks = [];
 
     this.visibilityAncestor = null;
-    this.visibilityObserver = null;
-    this.isVisible = null;
-    this.visibilityWatchers = [];
+    this.#visibilityObserver = null;
+    this.#isVisible = null;
+    this.#visibilityWatchers = [];
 
     if (elem != null) {
-      this.connect(elem);
+      this.#connect(elem);
     }
   }
 
-  assertElement(): HTMLElement {
+  #assertElement(): HTMLElement {
     if (this.element == null) {
       throw new Error(`Watcher not connected to an element`);
     }
     return this.element;
   }
 
-  assertVisibilityAncestor(): HTMLElement {
+  #assertVisibilityAncestor(): HTMLElement {
     if (this.visibilityAncestor == null) {
       throw new Error(`Watcher is missing a visibilityAncestor`);
     }
     return this.visibilityAncestor;
   }
 
-  isConnected(): boolean {
-    return this.element != null;
-  }
-
-  connect(element: HTMLElement, visibilityAncestor?: HTMLElement): void {
+  #connect(element: HTMLElement, visibilityAncestor?: HTMLElement): void {
     // Currently assuming that no selector matches more than one element.
     if (this.element != null) {
       // Watcher already connected.
@@ -115,37 +111,37 @@ export class Watcher {
       );
     }
 
-    for (const onCreatedCb of this.onCreatedCallbacks) {
+    for (const onCreatedCb of this.#onCreatedCallbacks) {
       const onRemovedCb = onCreatedCb(this.element);
       if (onRemovedCb) {
-        this.onRemovedCallbacks.push(onRemovedCb);
+        this.#onRemovedCallbacks.push(onRemovedCb);
       }
     }
 
-    for (const { selector, name, watcher } of this.nodeWatchers) {
+    for (const { selector, name, watcher } of this.#nodeWatchers) {
       for (const descElem of getDescendantsBy(this.element, selector, name)) {
-        watcher.connect(descElem, this.element);
+        watcher.#connect(descElem, this.element);
       }
     }
 
-    for (const callback of this.onTextChangedCallbacks) {
+    for (const callback of this.#onTextChangedCallbacks) {
       callback(this.element, this.element.textContent);
     }
 
-    for (const { name, callback } of this.onAttrChangedCallbacks) {
+    for (const { name, callback } of this.#onAttrChangedCallbacks) {
       callback(this.element, this.element.getAttribute(name));
     }
 
     // The visibilityObserver will trigger automatically if the element is visible
     // already. No need to handle visibilityWatchers here.
 
-    this.registerNodeObserver();
-    this.registerTextObserver();
-    this.registerAttrObservers();
-    this.registerVisibilityObserver();
+    this.#registerNodeObserver();
+    this.#registerTextObserver();
+    this.#registerAttrObservers();
+    this.#registerVisibilityObserver();
   }
 
-  disconnect(): void {
+  #disconnect(): void {
     if (this.element == null) {
       // Watcher already disconnected
       return;
@@ -159,102 +155,102 @@ export class Watcher {
       );
     }
 
-    for (const child of this.nodeWatchers) {
-      child.watcher.disconnect();
+    for (const child of this.#nodeWatchers) {
+      child.watcher.#disconnect();
     }
 
-    for (const callback of this.onTextChangedCallbacks) {
+    for (const callback of this.#onTextChangedCallbacks) {
       callback(this.element, undefined);
     }
 
-    for (const { callback } of this.onAttrChangedCallbacks) {
+    for (const { callback } of this.#onAttrChangedCallbacks) {
       callback(this.element, undefined);
     }
 
-    for (const child of this.visibilityWatchers) {
-      child.disconnect();
+    for (const child of this.#visibilityWatchers) {
+      child.#disconnect();
     }
 
-    this.deregisterNodeObserver();
-    this.deregisterTextObserver();
-    this.deregisterAttrObservers();
-    this.deregisterVisibilityObserver();
+    this.#deregisterNodeObserver();
+    this.#deregisterTextObserver();
+    this.#deregisterAttrObservers();
+    this.#deregisterVisibilityObserver();
 
-    while (this.onRemovedCallbacks.length > 0) {
-      const onRemovedCb = this.onRemovedCallbacks.shift()!;
+    while (this.#onRemovedCallbacks.length > 0) {
+      const onRemovedCb = this.#onRemovedCallbacks.shift()!;
       onRemovedCb();
     }
 
     this.element = null;
   }
 
-  registerNodeObserver(): void {
-    if (this.nodeObserver != null) {
+  #registerNodeObserver(): void {
+    if (this.#nodeObserver != null) {
       // Already registered.
       return;
     }
 
-    if (this.nodeWatchers.length === 0) {
+    if (this.#nodeWatchers.length === 0) {
       // No watchers, no need for an observer.
       return;
     }
 
-    const elem = this.assertElement();
+    const elem = this.#assertElement();
 
-    this.nodeObserver = new MutationObserver((mutations) => {
+    this.#nodeObserver = new MutationObserver((mutations) => {
       for (const mut of mutations) {
         for (const node of mut.addedNodes) {
-          for (const { selector, name, watcher } of this.nodeWatchers) {
+          for (const { selector, name, watcher } of this.#nodeWatchers) {
             for (const descElem of getSelfOrDescendantsBy(
               node,
               selector,
               name,
             )) {
-              watcher.connect(descElem, elem);
+              watcher.#connect(descElem, elem);
             }
           }
         }
 
         for (const node of mut.removedNodes) {
-          for (const { selector, name, watcher } of this.nodeWatchers) {
+          for (const { selector, name, watcher } of this.#nodeWatchers) {
             for (const _descElem of getSelfOrDescendantsBy(
               node,
               selector,
               name,
             )) {
-              watcher.disconnect();
+              watcher.#disconnect();
             }
           }
         }
       }
     });
 
-    this.nodeObserver.observe(elem, {
+    this.#nodeObserver.observe(elem, {
       subtree: true,
       childList: true,
     });
   }
 
-  registerTextObserver(): void {
-    if (this.textObserver != null) {
+  #registerTextObserver(): void {
+    if (this.#textObserver != null) {
       // Already registered.
       return;
     }
 
-    if (this.onTextChangedCallbacks.length === 0) {
+    if (this.#onTextChangedCallbacks.length === 0) {
       // No callbacks, no need for an observer.
       return;
     }
 
-    const elem = this.assertElement();
+    const elem = this.#assertElement();
 
-    this.textObserver = new MutationObserver((_mutations) => {
-      for (const callback of this.onTextChangedCallbacks) {
+    this.#textObserver = new MutationObserver((_mutations) => {
+      for (const callback of this.#onTextChangedCallbacks) {
         callback(elem, elem.textContent);
       }
     });
 
-    this.textObserver.observe(elem, {
+    this.#textObserver.observe(elem, {
       subtree: true,
       // This is needed when elements are replaced to update their text.
       childList: true,
@@ -262,10 +258,10 @@ export class Watcher {
     });
   }
 
-  registerAttrObservers(): void {
-    const elem = this.assertElement();
+  #registerAttrObservers(): void {
+    const elem = this.#assertElement();
 
-    for (const handler of this.onAttrChangedCallbacks) {
+    for (const handler of this.#onAttrChangedCallbacks) {
       if (handler.observer != null) {
         // Already registered.
         continue;
@@ -284,38 +280,38 @@ export class Watcher {
     }
   }
 
-  registerVisibilityObserver(): void {
-    if (this.visibilityObserver != null) {
+  #registerVisibilityObserver(): void {
+    if (this.#visibilityObserver != null) {
       // Already registered.
       return;
     }
 
-    if (this.visibilityWatchers.length === 0) {
+    if (this.#visibilityWatchers.length === 0) {
       // No watchers, no need for an observer.
       return;
     }
 
-    this.isVisible = false;
+    this.#isVisible = false;
 
-    const elem = this.assertElement();
-    const visibilityAncestor = this.assertVisibilityAncestor();
+    const elem = this.#assertElement();
+    const visibilityAncestor = this.#assertVisibilityAncestor();
 
-    this.visibilityObserver = new IntersectionObserver(
+    this.#visibilityObserver = new IntersectionObserver(
       (entries) => {
-        const oldVisible = this.isVisible;
+        const oldVisible = this.#isVisible;
 
         for (const entry of entries) {
-          this.isVisible = entry.isIntersecting;
+          this.#isVisible = entry.isIntersecting;
         }
 
-        if (this.isVisible !== oldVisible) {
-          if (this.isVisible) {
-            for (const watcher of this.visibilityWatchers) {
-              watcher.connect(elem, visibilityAncestor);
+        if (this.#isVisible !== oldVisible) {
+          if (this.#isVisible) {
+            for (const watcher of this.#visibilityWatchers) {
+              watcher.#connect(elem, visibilityAncestor);
             }
           } else {
-            for (const watcher of this.visibilityWatchers) {
-              watcher.disconnect();
+            for (const watcher of this.#visibilityWatchers) {
+              watcher.#disconnect();
             }
           }
         }
@@ -325,33 +321,33 @@ export class Watcher {
       },
     );
 
-    this.visibilityObserver.observe(elem);
+    this.#visibilityObserver.observe(elem);
   }
 
-  deregisterNodeObserver(): void {
-    if (this.nodeObserver == null) {
+  #deregisterNodeObserver(): void {
+    if (this.#nodeObserver == null) {
       // Already unregistered.
       return;
     }
 
     // Throwing away any pending events.
-    this.nodeObserver.disconnect();
-    this.nodeObserver = null;
+    this.#nodeObserver.disconnect();
+    this.#nodeObserver = null;
   }
 
-  deregisterTextObserver(): void {
-    if (this.textObserver == null) {
+  #deregisterTextObserver(): void {
+    if (this.#textObserver == null) {
       // Already unregistered.
       return;
     }
 
     // Throwing away any pending events.
-    this.textObserver.disconnect();
-    this.textObserver = null;
+    this.#textObserver.disconnect();
+    this.#textObserver = null;
   }
 
-  deregisterAttrObservers(): void {
-    for (const handler of this.onAttrChangedCallbacks) {
+  #deregisterAttrObservers(): void {
+    for (const handler of this.#onAttrChangedCallbacks) {
       if (handler.observer == null) {
         // Already unregistered.
         continue;
@@ -363,26 +359,26 @@ export class Watcher {
     }
   }
 
-  deregisterVisibilityObserver(): void {
-    if (this.visibilityObserver == null) {
+  #deregisterVisibilityObserver(): void {
+    if (this.#visibilityObserver == null) {
       // Already unregistered.
       return;
     }
 
     // Throwing away any pending events.
-    this.visibilityObserver.disconnect();
-    this.visibilityObserver = null;
+    this.#visibilityObserver.disconnect();
+    this.#visibilityObserver = null;
 
-    this.isVisible = null;
+    this.#isVisible = null;
   }
 
   onCreated(onCreatedCb: (elem: HTMLElement) => (() => void) | void): Watcher {
-    this.onCreatedCallbacks.push(onCreatedCb);
+    this.#onCreatedCallbacks.push(onCreatedCb);
 
     if (this.element != null) {
       const onRemovedCb = onCreatedCb(this.element);
       if (onRemovedCb) {
-        this.onRemovedCallbacks.push(onRemovedCb);
+        this.#onRemovedCallbacks.push(onRemovedCb);
       }
     }
 
@@ -392,14 +388,14 @@ export class Watcher {
   descendant(selector: SelectorType, name: string): Watcher {
     const watcher = new Watcher(`${this.name} → ${name}`);
 
-    this.nodeWatchers.push({ selector, name, watcher });
+    this.#nodeWatchers.push({ selector, name, watcher });
 
     if (this.element != null) {
       for (const descElem of getDescendantsBy(this.element, selector, name)) {
-        watcher.connect(descElem, this.element);
+        watcher.#connect(descElem, this.element);
       }
 
-      this.registerNodeObserver();
+      this.#registerNodeObserver();
     }
 
     return watcher;
@@ -420,18 +416,18 @@ export class Watcher {
   visible(): Watcher {
     const watcher = new Watcher(`${this.name} (visible)`);
 
-    this.visibilityWatchers.push(watcher);
+    this.#visibilityWatchers.push(watcher);
 
     if (this.element != null) {
-      const visibilityAncestor = this.assertVisibilityAncestor();
+      const visibilityAncestor = this.#assertVisibilityAncestor();
 
-      if (this.isVisible) {
+      if (this.#isVisible) {
         // The observer is already registered, connect manually as it wouldn't
         // trigger otherwise.
-        watcher.connect(this.element, visibilityAncestor);
+        watcher.#connect(this.element, visibilityAncestor);
       }
 
-      this.registerVisibilityObserver();
+      this.#registerVisibilityObserver();
     }
 
     return watcher;
@@ -440,11 +436,11 @@ export class Watcher {
   /// `null` implies null textContent. `undefined` implies that the watcher is
   /// being disconnected.
   text(callback: OnTextChangedCallback): Watcher {
-    this.onTextChangedCallbacks.push(callback);
+    this.#onTextChangedCallbacks.push(callback);
     if (this.element != null) {
       callback(this.element, this.element.textContent);
 
-      this.registerTextObserver();
+      this.#registerTextObserver();
     }
 
     return this;
@@ -453,12 +449,12 @@ export class Watcher {
   /// `null` implies no such attribute. `undefined` implies that the watcher is
   /// being disconnected.
   attr(name: string, callback: OnAttrChangedCallback): Watcher {
-    this.onAttrChangedCallbacks.push({ name, callback, observer: null });
+    this.#onAttrChangedCallbacks.push({ name, callback, observer: null });
 
     if (this.element != null) {
       callback(this.element, this.element.getAttribute(name));
 
-      this.registerAttrObservers();
+      this.#registerAttrObservers();
     }
 
     return this;
